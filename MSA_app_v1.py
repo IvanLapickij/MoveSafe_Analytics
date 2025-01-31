1e01a6177280f77364d8aca74cd76500acdf7c54
Ivan Lapickij <ivan.lapickij@gmail.com>
2025-01-31 03:34:56 +0000

    added MSA football model

--- MSA_app_v1.py
+++ MSA_app_v1.py
@@ -1,19 +1,43 @@
-import cv2
+
+#reference video https://blog.roboflow.com/getting-started-with-roboflow/
+#reference documentation https://docs.roboflow.com/workflows/deploy-a-workflow
+#reference Roboflow football project video https://www.youtube.com/watch?v=aBVGKoNZQUw&t
+
+#1.Run Docker server clever_noether
+
+#2.Run comman to project camera view to pipeline using terminal
+#ffmpeg -f dshow -rtbufsize 256M -i video="Integrated Camera" -preset ultrafast -tune zerolatency -f flv rtmp://192.168.0.170:1935/stream
+
+#3. run python appclication
+# Import the InferencePipeline object
 from inference import InferencePipeline
 from inference.core.interfaces.stream.sinks import render_boxes
 
-# Define the RTMP stream URL
+# Video samples
+# RTMP_STREAM_URL = "videos/bundesliga1.mp4"
+
+#Lenovo
 RTMP_STREAM_URL = "rtmp://127.0.0.1:1935/stream"
 
-# Create an inference pipeline object
+#MSI
+# RTMP_STREAM_URL = "rtmp://192.168.0.170:1935/stream"
+
+#do something with the predictions of each frame
+def my_sink(result, video_frame):
+    
+    # Visualize detections on the video stream
+    render_boxes(result, video_frame)
+    print(result) # prints result of each frame
+    
+
+# initialize a pipeline object
 pipeline = InferencePipeline.init(
+    api_key="tvZVhjN9hMWkURbVo84w",
     model_id="football-players-detection-3zvbc/12",  # Model ID
-    video_reference=RTMP_STREAM_URL,  # RTMP stream as the video source
-    api_key="tvZVhjN9hMWkURbVo84w",  # Roboflow API key
+    video_reference=RTMP_STREAM_URL, # Path to video, RSTP stream, device id (int, usually 0 for built in webcams), or RTSP stream url
+    on_prediction=my_sink
 )
+pipeline.start() #start the pipeline
+pipeline.join() #wait for the pipeline thread to finish
 
-# Start the pipeline
-pipeline.start()
 
-# Wait for the pipeline to finish
-pipeline.join()
